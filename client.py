9359758042ae3050021b86ca1f750c97772040f7
VinWare <28603906+VinWare@users.noreply.github.com>
2023-01-06 18:23:55 -0800

    Added constant values for stock names of which ratio is being calculated to avoid magic constants Added comments

--- client.py
+++ client.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
@@ -50,14 +50,17 @@ def getRatio(price_a, price_b):
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    # Stock names of which we must calculate ratio
+    stock1, stock2 = 'ABC', 'DEF'
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        # Holds calculated price for each stock name
         prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
+        print("Ratio %s" % getRatio(prices[stock1], prices[stock2]))
